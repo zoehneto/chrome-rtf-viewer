a6099890cda985579c540ba2e9d8afbba385ff19
Tom Z?hner <tom.zoehner@gmail.com>
2017-09-14 19:42:47 +0200

    Check content-disposition = attachment for file names indicative of rtf files

--- rtfHandler.js
+++ rtfHandler.js
@@ -4,6 +4,15 @@ function getViewerURL(fileUrl) {
     return chrome.extension.getURL('viewer/viewer.html') + '?file=' + encodeURIComponent(fileUrl);
 }
 
+function getHeader(details, headerName) {
+    return details.responseHeaders.find(function(element){
+        if(element.name.toLowerCase() === headerName){
+            return element;
+        }
+        return false;
+    });
+}
+
 function contentTypeContains(contentType, string){
     if(contentType.value.indexOf(string) > -1){
         return true;
@@ -18,20 +27,36 @@ function urlIndicatesRtf(url){
     return url.toLowerCase().endsWith('.rtf');
 }
 
-function isRtfFile(details) {
-    var contentType = details.responseHeaders.find(function(element){
-        if(element.name.toLowerCase() === 'content-type'){
-            return element;
+function contentDispositionIndicatesRtf(contentDisposition){
+    if (contentDisposition.value.toLowerCase().startsWith('attachment')) {
+        var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+        var filename = filenameRegex.exec(contentDisposition.value)[1];
+        if (filename) {
+            return filename.toLowerCase().endsWith('.rtf');
         }
-        return false;
-    });
+    }
+    return false;
+}
+
+function isRtfFile(details) {
+    var contentType = getHeader(details, 'content-type');
+
+    var contentDisposition = getHeader(details, 'content-disposition');
+
+    var isRtf = false;
 
     if (contentType) {
-        return (contentTypeContains(contentType, 'application/rtf') ||
+        isRtf = contentTypeContains(contentType, 'application/rtf') ||
             contentTypeContains(contentType, 'text/rtf') ||
             contentTypeContains(contentType, 'text/richtext') ||
-            urlIndicatesRtf(details.url));
+            urlIndicatesRtf(details.url);
     }
+
+    if (contentDisposition) {
+        isRtf = isRtf || contentDispositionIndicatesRtf(contentDisposition);
+    }
+
+    return isRtf;
 }
 
 chrome.webRequest.onHeadersReceived.addListener(
