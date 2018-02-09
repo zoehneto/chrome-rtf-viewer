60c52732cf5d3511f17c1d1efb56fb96e9b878a5
Tom Z?hner <tom.zoehner@gmail.com>
2018-02-09 17:01:34 +0100

    Fix opening upload page

--- background.js
+++ background.js
@@ -1,9 +1,5 @@
 'use strict';
 
-function getViewerURL() {
-    return chrome.extension.getURL('viewer/viewer.html');
-}
-
 chrome.browserAction.onClicked.addListener(function(activeTab) {
-    chrome.tabs.create({ url: getViewerURL() });
+    chrome.tabs.create({ url: chrome.extension.getURL('viewer/viewer.html') });
 });
